f807cd2156b0623a35b67964df59642925f15d20
Akash UR <akashur000@gmail.com>
2026-02-15 09:14:04 +0530

    Time: 1 ms (99.65%) | Memory: 43.5 MB (70.73%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb=new StringBuilder();
+        int temp=0;
+        int i=a.length()-1;
+        int j=b.length()-1;
+        while(i>=0 || j>=0 || temp==1){
+            if(i>=0) temp+=a.charAt(i--) - '0';
+            if(j>=0) temp+=b.charAt(j--) - '0';
+            sb.append(temp % 2);
+            temp/=2;
+        }  
+      return sb.reverse().toString();
+    }
+}
