38466988445e425577a087eef47028c10c06dbbe
Akash UR <akashur000@gmail.com>
2026-06-25 16:10:10 +0530

    Time: 7 ms (99.61%) | Memory: 47.4 MB (84.94%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -2,11 +2,21 @@ class Solution {
     public boolean winnerOfGame(String colors) {
         int aliceCount = 0;
         int bobCount = 0;
-        for (int i = 2; i < colors.length(); i++) {
-            if (colors.charAt(i) == 'A' && colors.charAt(i - 1) == 'A' && colors.charAt(i - 2) == 'A')
-                aliceCount++;
-            if (colors.charAt(i) == 'B' && colors.charAt(i - 1) == 'B' && colors.charAt(i - 2) == 'B')
-                bobCount++;
+        int consecutiveA = 0;
+        int consecutiveB = 0;
+        for (char ch : colors.toCharArray()) {
+
+            if (ch == 'A') {
+                consecutiveB = 0;
+                consecutiveA++;
+                if (consecutiveA >= 3)
+                    aliceCount++;
+            } else {
+                consecutiveA = 0;
+                consecutiveB++;
+                if (consecutiveB >= 3)
+                    bobCount++;
+            }
         }
         return aliceCount > bobCount;
     }
