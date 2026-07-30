9afa6888e6cf44e2aa17c3c441c520d703d9cf58
Akash UR <akashur000@gmail.com>
2026-07-30 09:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (78.00%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,6 +1,5 @@
 class Solution {
     public String truncateSentence(String s, int k) {
-        String[] arr = s.split(" ");
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ' ')
                 k--;
